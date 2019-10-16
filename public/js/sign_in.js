ad02968fef620f8e4cb8a532dd51ec7cd34f90d0
yuhantsao9999 <yuhantsao9999@gmail.com>
2019-10-16 21:26:05 +0800

    update fblogin

--- public/js/sign_in.js
+++ public/js/sign_in.js
@@ -1,45 +1,47 @@
 //native
 document.getElementById("sumbit").addEventListener("click", function() {
-    if (document.getElementById("email").value == "") {
-        alert("! 請輸入電子郵件地址");
-    } else if (document.getElementById("password").value == '') {
-        alert("! 請輸入密碼");
-    } else {
-        var xml = new XMLHttpRequest;
-        xml.open('post', '/user/signin', true);
-        xml.setRequestHeader('content-type', 'application/json')
-            // var name = document.getElementById("name").value;
-        var email = document.getElementById("email").value;
-        var password = document.getElementById("password").value;
-        // console.log(name, email, password)
-        var profile = {
-                provider: "native",
-                name: name,
-                email: email,
-                password: password
-            }
-            // console.log(profile)
-        xml.send(JSON.stringify(profile));
-        xml.onload = function() {
-            // console.log(xml.responseText)
-            if (xml.responseText == "error") {
-                alert("please sign up first!")
-                window.location.assign('./sign_up.html')
-            } else {
-                var accessToken = JSON.parse(xml.responseText).data.access_token;
-                // console.log(accessToken);
-                localStorage.setItem("accessToken", accessToken);
-                alert("sign in successful")
-                window.location.assign('./profile.html')
-            }
+    var xml = new XMLHttpRequest;
+    xml.open('post', '/user/signin', true);
+    xml.setRequestHeader('content-type', 'application/json')
+        // var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    // console.log(name, email, password)
+    var profile = {
+            provider: "native",
+            name: name,
+            email: email,
+            password: password
+        }
+        // console.log(profile)
+    xml.send(JSON.stringify(profile));
+    xml.onload = function() {
+        // console.log(xml.responseText)
+        if (xml.responseText == "error") {
+            alert("please sign up first!")
+            window.location.assign('./sign_up.html')
+        } else {
+            var accessToken = JSON.parse(xml.responseText).data.access_token;
+            // console.log(accessToken);
+            localStorage.setItem("accessToken", accessToken);
+            alert("sign in successful")
+            window.location.assign('./profile.html')
         }
     }
 });
 
 
 //FB
+// This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
     if (response.status === 'connected') {
+        // Logged into your app and Facebook.
         testAPI();
         // console.log("gggggg" + response.authResponse.accessToken);
         var accessToken = response.authResponse.accessToken;
@@ -67,8 +69,9 @@ function statusChangeCallback(response) {
             'into this app.';
     }
 }
-
-
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
 function checkLoginState() {
     FB.getLoginStatus(function(response) {
         statusChangeCallback(response);
@@ -77,24 +80,42 @@ function checkLoginState() {
 window.fbAsyncInit = function() {
     FB.init({
         appId: '513734669198278',
-        xfbml: true,
-        version: 'v4.0'
+        cookie: true, // enable cookies to allow the server to access 
+        // the session
+        xfbml: true, // parse social plugins on this page
+        version: 'v3.3' // The Graph API version to use for the call
     });
-    FB.AppEvents.logPageView();
+    // Now that we've initialized the JavaScript SDK, we call 
+    // FB.getLoginStatus().  This function gets the state of the
+    // person visiting this page and can return one of three states to
+    // the callback you provide.  They can be:
+    //
+    // 1. Logged into your app ('connected')
+    // 2. Logged into Facebook, but not your app ('not_authorized')
+    // 3. Not logged into Facebook and can't tell if they are logged into
+    //    your app or not.
+    //
+    // These three cases are handled in the callback function.
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
 };
-
+// Load the SDK asynchronously
 (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
+    if (d.getElementById(id)) return;
     js = d.createElement(s);
     js.id = id;
     js.src = "https://connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-
-
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
 function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
-        document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!';
+        console.log('Successful login for: ' + response.name);
+        document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
     });
 }
--- public/js/sign_out.js
+++ public/js/sign_out.js
@@ -2,8 +2,7 @@ function sign_out_icon() {
     var sign_li = document.createElement("li");
     var signContent = document.createTextNode("登出");
     var sign_a = document.createElement("a");
-    // sign_a.setAttribute("onClick", "sign_out()");
-    sign_a.setAttribute("onClick", "FB_logout(); ");
+    sign_a.setAttribute("onClick", "sign_out()");
     sign_a.appendChild(signContent)
     sign_li.appendChild(sign_a)
     document.getElementById("clearfix").appendChild(sign_li)
@@ -12,12 +11,6 @@ sign_out_icon();
 //log out
 function sign_out() {
     localStorage.removeItem('accessToken');
-    alert("帳號登出囉!")
-    window.location.replace("./sign_in.html");
-}
-
-function FB_logout() {
-    window.location.reload()
     alert("帳號登出囉!")
     window.location.replace("./sign_in.html");
 }
